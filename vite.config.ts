e05342851c19a982d8f3ea6de0676dae74d429ca
ManuelWestermeier <westermeier111@gmail.com>
2024-06-28 18:14:26 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "wsnet-client-react-test",
+  base: "/wsnet-client-react-test/docs/",
   build: { outDir: "docs" }
 })
